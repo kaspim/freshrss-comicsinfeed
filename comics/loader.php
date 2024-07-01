42ea4127f3d70ae508ac197ef84d195e440fcf7f
Martin Kašpar <hello@martinkaspar.net>
2024-07-01 16:42:18 +0200

    Modified loader to autoload parsers

--- comics/loader.php
+++ comics/loader.php
@@ -1,26 +1,10 @@
 <?php
 
-//buttersafe
-require_once __DIR__ . '/buttersafe.php';
-//the awkward yeti
-require_once __DIR__ . '/theawkwardyeti.php';
-//buni
-require_once __DIR__ . '/buni.php';
-//xkcd
-require_once __DIR__ . '/xkcd.php';
-//penny arcade
-require_once __DIR__ . '/penny-arcade.php';
-//explosm
-require_once __DIR__ . '/explosm.php';
-//monster under the bed
-require_once __DIR__ . '/monsterunderbed.php';
-//Sluggy Freelance
-require_once __DIR__ . '/sluggy.php';
-//Girl Genius
-require_once __DIR__ . '/girlgenius.php';
-//Questionable Content
-require_once __DIR__ . '/questionablecontent.php';
-//Flipside
-require_once __DIR__ . '/flipside.php';
+$files = array_diff(scandir(__DIR__), [basename(__FILE__), '.', '..']);
+$files = array_filter($files, function ($file) {
+    return pathinfo($file, PATHINFO_EXTENSION) === 'php';
+});
 
-?>
+foreach ($files as $file) {
+    require_once __DIR__ . '/' . $file;
+}
